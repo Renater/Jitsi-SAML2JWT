5f6cfc087dcafbe6209bf6d2b6048e8076675c07
daimoc <damien.fetis@renater.fr>
2023-01-06 17:34:33 +0100

    Refactor docker file to use env file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+export config='$config'
+
+echo "ServerName $SERVER_NAME"
+echo "Generating config File from template and jitsi-SAML2JWT.env file"
+
+echo "PHP Token file"
+cat ../config/template/config.php | envsubst > ../config/config.php
+
+if [ -z  "$SP_ENTITY_ID" ]
+then
+echo "Apache File"
+cat ../config/template/jwtgenerator.conf | envsubst > ../config/jitsi-auth.conf
+else
+echo "Apache File"
+cat ../config/template/jwtgenerator-sp.conf | envsubst > ../config/jitsi-auth.conf
+echo "Shibboleth file"
+cat ../config/template/$SHIBBOLETH_TEMPLATE_XML | envsubst > ../config/shibboleth2.xml
+fi
+
+echo "Generation Done"
+
 #Start Shibboleth
 service shibd start
 
