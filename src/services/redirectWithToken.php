f6698fe3d8ba53427c8596bb7455a9805e18009f
daimoc <damien.fetis@renater.fr>
2023-09-26 14:33:38 +0200

    Add Extra Param for the Jitsi Iframe API

--- src/services/redirectWithToken.php
+++ src/services/redirectWithToken.php
@@ -15,7 +15,14 @@ try {
     $jsonResp =  $tokenGenerator->getToken($_SERVER,$_REQUEST);
     $room=$_GET['room'];
     $tenant=$_GET['tenant'];
-    $URL="https://".$config['jitsi_domain']."/".$tenant.$room."?jwt=".$jsonResp;
+    
+    $extra="#";
+    if(array_key_exists('jitsi_meet_external_api_id',$_REQUEST)){
+        $extra.="jitsi_meet_external_api_id=";
+        $extra.=$_REQUEST['jitsi_meet_external_api_id'];
+    }
+         
+    $URL="https://".$config['jitsi_domain']."/".$tenant.$room."?jwt=".$jsonResp.$extra;
     header("Location: $URL", true, 302); 
     return;
 } catch (Exception $e){
